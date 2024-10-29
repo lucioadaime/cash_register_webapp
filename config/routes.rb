cb230dde375498d833bf657038f3263da53bf04a
Lucio Adaime <ladaime@miwendo.com>
2024-10-29 12:14:16 +0100

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   root 'main#menu'  # The main menu
   get 'menu', to: 'main#menu', as: 'menu'
+  post 'checkout', to: 'main#checkout', as: 'checkout_cart'
   # Define routes for cart actions
   resource :cart, only: [ :show ] do
     post 'add_product', to: 'carts#add_product'  # POST for adding product to cart
     delete 'remove_product', to: 'carts#remove_product'  # DELETE for removing product
-    post 'checkout', to: 'carts#checkout'  # POST for checkout
+    post 'checkout', to: 'main#checkout', as: 'checkout_cart'
   end
 end
